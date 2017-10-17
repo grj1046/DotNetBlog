dc238dfb400c716fa8eee26e93c636e26a567516
RenJun Guo <RenJun Guo>
2017-10-17 17:01:06 +0800

    add extensions file

---
+++ src/DotNetBlog/Extensions/EmailSenderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace DotNetBlog.Services
+{
+    public static class EmailSenderExtensions
+    {
+        public static Task SendEmailConfirmationAsync(this IEmailSender emailSender, string email, string link)
+        {
+            string strSubject = "Confirm your email";
+            string strMessage = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(link)}'>clicking here</a>.";
+            return emailSender.SendEmailAsync(email, strSubject, strMessage);
+        }
+        public static Task SendResetPasswordAsync(this IEmailSender emailSender, string email, string link)
+        {
+            string strSubject = "Reset Password";
+            string strMessage = $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(link)}'>clicking here</a>.";
+            return emailSender.SendEmailAsync(email, strSubject, strMessage);
+        }
+    }
+}
