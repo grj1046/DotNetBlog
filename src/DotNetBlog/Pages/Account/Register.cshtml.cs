37ea7f85fca4348b4f7ad6e1419fe63bd8eab666
RenJun Guo <RenJun Guo>
2017-10-18 11:40:55 +0800

    register file

---
+++ src/DotNetBlog/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetBlog.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public void OnGet(string returnUrl = null)
+        {
+            this.ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            this.ReturnUrl = returnUrl;
+            if (this.ModelState.IsValid)
+            {
+                if (Input.Email == "guorenjun@outlook.com" && Input.Password == "123456")
+                {
+                    //create user when pass authentication
+                    //send confirm email
+                }
+                await Task.CompletedTask;
+                return LocalRedirect(Url.GetLocalUrl(returnUrl));
+            }
+
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at lease {2} and at max {1} characters long.", MinimumLength = 2)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [Required]
+            [Display(Name = "Confirm password")]
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+    }
+}
