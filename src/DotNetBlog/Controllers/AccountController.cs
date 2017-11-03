6cd6f6b8ec475111c3e05f9eb8c47cdb7c7b0d02
grj1046 <guorenjun@outlook.com>
2017-11-03 18:31:26 +0800

    add SignedOut Lockout Page

---
+++ src/DotNetBlog/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotNetBlog.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetBlog.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly DotNetBlogDbContext DbContext;
+        private readonly ILogger Logger;
+
+        public AccountController(DotNetBlogDbContext dbContext, ILogger<AccountController> logger)
+        {
+            this.DbContext = dbContext;
+            this.Logger = logger;
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await this.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            this.Logger.LogInformation("User Logged out.");
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ src/DotNetBlog/Pages/Account/SignedOut.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetBlog.Pages.Account
+{
+    public class SignedOutModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToPage("/Inddex");
+            }
+            return Page();
+        }
+    }
+}
