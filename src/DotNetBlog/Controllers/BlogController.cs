8ff11e8fb137fd24dc27280730c41f246ed7331a
grj1046 <guorenjun@outlook.com>
2017-12-04 22:20:59 +0800

    post comment

---
+++ src/DotNetBlog/Controllers/BlogController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotNetBlog.Models;
+using System.Security.Claims;
+
+namespace DotNetBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public GuorjAccountDbContext DbAccount { get; set; }
+        public GuorjBlogDbContext DbBlog { get; set; }
+
+        public BlogController(GuorjAccountDbContext dbAccount, GuorjBlogDbContext dbBlog)
+        {
+            this.DbAccount = dbAccount;
+            this.DbBlog = dbBlog;
+        }
+
+        [Route("/Blog/Post/AddComment")]
+        public async Task<JsonResult> AddComment(Guid postID, Guid contentID, [FromForm]CommentResult commentResult)
+        {
+            if (postID == Guid.Empty)
+                throw new ArgumentNullException("PostID");
+            if (contentID == Guid.Empty)
+                throw new ArgumentNullException("ContentID");
+            if (!this.User.Identity.IsAuthenticated && string.IsNullOrEmpty(commentResult.UserEmail))
+                throw new UnauthorizedAccessException("User not Authenticated and UserEmail is empty also.");
+
+            Comment comment = new Comment();
+            comment.PostID = postID;
+            comment.IsDeleted = false;
+            comment.CreateAt = DateTime.Now;
+            comment.Content = commentResult.Comment;
+            comment.ContentID = contentID;
+            comment.IsDeleted = false;
+            if (this.User.Identity.IsAuthenticated)
+            {
+                var userID = this.User.GetUserID();
+                var userName = this.User.Identity.Name;
+                comment.UserID = userID;
+                comment.UserName = userName;
+            }
+            else if (!string.IsNullOrEmpty(commentResult.UserEmail))
+            {
+                comment.UserEmail = commentResult.UserEmail;
+                comment.UserName = commentResult.UserName;
+            }
+
+            await this.DbBlog.Comments.AddAsync(comment);
+            await this.DbBlog.SaveChangesAsync();
+            return Json(commentResult);
+        }
+
+        public class CommentResult
+        {
+            public Guid CommentID { get; set; }
+            public string Comment { get; set; }
+            public string UserName { get; set; }
+            public string UserEmail { get; set; }
+            public Guid ParentCommentID { get; set; }
+            public DateTime CreateAt { get; set; }
+        }
+    }
+}
--- src/DotNetBlog/Pages/Blog/Post.cshtml.cs
+++ src/DotNetBlog/Pages/Blog/Post.cshtml.cs
@@ -26,7 +26,7 @@ namespace DotNetBlog.Pages.Blog
             if (string.IsNullOrEmpty(p) || p.Length > 256)
                 return NotFound();
             //get guid
-            var query = from post in this.DbBlog.Posts.Include(a => a.Tags)
+            var query = from post in this.DbBlog.Posts.Include(a => a.Tags).Include(a => a.Comments)
                         join postContent in this.DbBlog.PostContents
                         on post.PostID equals postContent.PostID
                         where post.URL == p && post.CurrentContentID == postContent.PostContentID
@@ -39,11 +39,20 @@ namespace DotNetBlog.Pages.Blog
                             Tags = post.Tags,
                             EditorType = postContent.EditorType,
                             MD5Hash = postContent.MD5Hash,
+                            ContentID = postContent.PostContentID,
                             Content = postContent.Content,
-                            ContentCreateAt = postContent.CreateAt
+                            ContentCreateAt = postContent.CreateAt,
+                            Comments = post.Comments.Where(a => !a.IsDeleted)
+                                .Select(a => new PostCommentViewModel()
+                                {
+                                    CommentID = a.CommentID,
+                                    ParentCommentID = a.ParentCommentID,
+                                    Content = a.Content,
+                                    UserName = a.UserName
+                                })
                         };
 
-            var currPost = await query.FirstOrDefaultAsync();
+            var currPost = await query.AsNoTracking().FirstOrDefaultAsync();
             if (currPost == null)
                 return NotFound();
             this.Post = currPost;
@@ -62,10 +71,22 @@ namespace DotNetBlog.Pages.Blog
 
             public IEnumerable<PostTag> Tags { get; set; }
 
+            public Guid ContentID { get; set; }
             public string Content { get; set; }
             public EditorType EditorType { get; set; }
             public string MD5Hash { get; internal set; }
             public DateTime ContentCreateAt { get; set; }
+
+            public IEnumerable<PostCommentViewModel> Comments { get; set; }
+        }
+
+        public class PostCommentViewModel
+        {
+            public Guid CommentID { get; set; }
+            public string Content { get; set; }
+            public DateTime CreateAt { get; set; }
+            public Guid ParentCommentID { get; set; }
+            public string UserName { get; set; }
         }
     }
 }
