27df7190ffd53c4c7cc295cbec89b23cfa9a8f54
grj1046 <guorenjun@outlook.com>
2019-12-10 18:53:25 +0800

    Reset Password

---
+++ src/DotNetBlog/Pages/Account/Reset.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetBlog
+{
+    public class ResetModel : PageModel
+    {
+        private readonly IDbConnectionFactory db;
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public ResetModel(IDbConnectionFactory dotNetBlogDb)
+        {
+            this.db = dotNetBlogDb;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+
+    public class InputModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
